681a2ee0881d6a04157f754a127010eda13b7856
BudzynV <oskar.budzynski99@gmail.com>
2022-03-02 11:16:29 +0100

    Ósme zasilenie

--- src/pl/lublin/wsei/java/cwiczenia/Main.java
+++ src/pl/lublin/wsei/java/cwiczenia/Main.java
@@ -1,14 +1,27 @@
 package pl.lublin.wsei.java.cwiczenia;
 
+import java.util.Random;
+
 public class Main {
 
     public static void main(String[] args) {
+        int[] liczby = new int[30];
+        Random rnd = new Random();
+
+        for(int i = 0; i < 30; i++)
+            liczby[i] = rnd.nextInt();
+
+        int mx = Integer.MIN_VALUE;
+        int mn = Integer.MAX_VALUE;
+        long avg = 0;
+        for(int l : liczby)
+        {
+            System.out.println(l);
+            if (l < mn) mn = 1;
+            if (l > mx) mx = 1;
+            avg += 1;
+        }
+        System.out.printf("MIN = %d, MAX = %d, AVG = %f", mn, mx,(float)avg/liczby.length);
     }
-    public static String leftPad(String aText, char aChar, int aWidth)
-    {
-     String res = aText;
-     for (int i = 0; i < aWidth - aText.length(); i++)
-         res = aChar + res;
-     return res;
-    }
+
 }
